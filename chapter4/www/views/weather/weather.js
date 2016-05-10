1c73191a5f80abe275882104ab516d43c1c0235f
John McIntosh II <jmci10@hotmail.com>
2016-05-10 11:17:24 +0000

    finish weather controller

--- chapter4/www/views/weather/weather.js
+++ chapter4/www/views/weather/weather.js
@@ -1,15 +1,17 @@
 angular.module("App")
 .controller("WeatherController", function ($scope, $http) {
-  var directions = [];
-  $http.get("")
+  var directions = ['N', 'NE', 'E', 'SE', 'S', 'SW', 'W', 'NW'];
+  $http.get("https://ionic-in-action-api.herokuapp.com/weather")
 .success(function (weather) {
   $scope.weather = weather;
 }).error(function (err) {
   }); 
   
-  $scope.getDirection = function (degreee) {
-    if (degree > 33) {
-      
+  $scope.getDirection = function (degree) {
+    if (degree > 338) {
+      degree = 360 - degree;
     }
+    var index = Math.floor((degree + 22) / 45);
+    return directions[index]
   };
 });
