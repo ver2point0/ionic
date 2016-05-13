b059c1fa9b2731520fd11ac1b320365bb1c759cc
John McIntosh II <jmci10@hotmail.com>
2016-05-13 03:54:21 +0000

    add loading component

--- chapter4/www/views/weather/weather.js
+++ chapter4/www/views/weather/weather.js
@@ -1,10 +1,17 @@
 angular.module("App")
-.controller("WeatherController", function ($scope, $http) {
+.controller("WeatherController", function ($scope, $http, $ionicLoading) {
   var directions = ['N', 'NE', 'E', 'SE', 'S', 'SW', 'W', 'NW'];
+  
+  $ionicLoading.show();
   $http.get("https://ionic-in-action-api.herokuapp.com/weather")
 .success(function (weather) {
   $scope.weather = weather;
+  $ionicLoading.hide();
 }).error(function (err) {
+  $ionicLoading.show({
+    template: "Could not load weather. Please try again later.",
+    duration: 3000
+  });
   }); 
   
   $scope.getDirection = function (degree) {
