5241fa20fb5f2ee620ff86064fe3b126ed34d112
John McIntosh II <jmci10@hotmail.com>
2016-05-23 13:30:00 +0000

    add restaurant state

--- chapter4/www/js/app.js
+++ chapter4/www/js/app.js
@@ -1,29 +1,40 @@
 angular.module('App', ['ionic'])
-  .config(function ($stateProvider, $urlRouterProvier) {
-    $stateProvider.state("home", {
+
+.config(function ($stateProvider, $urlRouterProvier) {
+  
+  $stateProvider
+    .state("home", {
       url: "/home",
       templateUrl: "views/home/home.html"
+    })
+    .state("reservation", {
+      url: "/reservation",
+      controller: "ReservationController",
+     templateUrl: "views/reservation/reservation.html"
+    })
+    .state("weather", {
+      url: "/weather",
+      controller: "WeatherController",
+      templateUrl: "views/weather/weather.html"
+    })
+    .state("restaurants", {
+      url: "/restaurants",
+      controller: "RestaurantsController",
+      templateUrl: "views/restaurants/restaurants.html"
     });
-    $urlRouterProvier.otherwise("/home");
-  })
-  .state("reservation", {
-    url: "/reservation",
-    controller: "ReservationController",
-    templateUrl: "views/reservation/reservation.html"
-  })
-  .state("weather", {
-    url: "/weather",
-    controller: "WeatherController",
-    templateUrl: "views/weather/weather.html"
-  })
-  .run(function($ionicPlatform) {
-    $ionicPlatform.ready(function() {
-      if(window.cordova && window.cordova.plugins.Keyboard) {
-        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      }
-      if(window.StatusBar) {
-        StatusBar.styleDefault();
-      }
-    });
-  })
+    
+  $urlRouterProvier.otherwise("/home");
+  
+})
+  
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+})
   
