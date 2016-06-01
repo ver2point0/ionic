96c40580c8601ebe85646de67b7c8bcaad1b094a
John McIntosh II <jmci10@hotmail.com>
2016-06-01 13:20:59 +0000

    add tabs state

--- chapter5/www/js/app.js
+++ chapter5/www/js/app.js
@@ -1,5 +1,18 @@
 angular.module('App', ['ionic'])
 
+.config(function ($stateProvider, $urlRouteProvider) {
+
+  $stateProvider
+    .state("tabs", {
+      url: "/tabs",
+      abstract: true,
+      templateUrl: "views/tab/tabs.html"
+    });
+    
+    $urlRouteProvider.otherwise("/tabs");
+})
+
+
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     if(window.cordova && window.cordova.plugins.Keyboard) {
