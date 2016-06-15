075cb270d066078bf204ec7c9dffbbd16bf0f00e
John McIntosh II <jmci10@hotmail.com>
2016-06-15 01:57:44 +0000

    finish rates controller

--- chapter5/www/views/rates/rates.js
+++ chapter5/www/views/rates/rates.js
@@ -1,4 +1,17 @@
 angular.module("App")
-.controller("RatesController", function($scope, $http, $ionicPopover, Currencies) {
+.controller("RatesController", function($scope, $http, Currencies) {
+
+  $scope.currencies = Currencies;
   
+  $scope.load = function () {
+    $http.get("https://api.bitcoinaverage.com/ticker/all").success(
+      function (tickers) {
+        angular.forEach($scope.currencies, function (currency) {
+          currency.ticker = tickers[currency.code];
+          currency.ticker.timestamp = new Date(currency.ticker.timestamp);
+        });
+      });
+  };
+  
+  $scope.load();
 });
