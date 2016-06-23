52af85b103c27feebf2980080ac57c5a89cb0b6c
John McIntosh II <jmci10@hotmail.com>
2016-06-23 12:39:32 +0000

    start detail controller

---
+++ chapter5/www/views/detail/detail.js
@@ -0,0 +1,15 @@
+angular.module("App")
+.controller("DetailController", function ($scope, $stateParams, $state, Currencies) {
+  
+  angular.forEach(Currencies, function(currency) {
+    if () {
+      
+    }
+  });
+  
+  
+  if () {
+    
+  }
+  
+});
