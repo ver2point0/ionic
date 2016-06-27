11760c847a606642c0f41e72204a30e06fb3826a
John McIntosh II <jmci10@hotmail.com>
2016-06-27 12:59:48 +0000

    finish detail controller

--- chapter5/www/views/detail/detail.js
+++ chapter5/www/views/detail/detail.js
@@ -2,14 +2,13 @@ angular.module("App")
 .controller("DetailController", function ($scope, $stateParams, $state, Currencies) {
   
   angular.forEach(Currencies, function(currency) {
-    if () {
-      
+    if (currency.code === $stateParams.currency) {
+      $scope.currency = currency;
     }
   });
   
-  
-  if () {
-    
+  if (angular.isUndefined($scope.currency.ticker)) {
+    $state.go("tabs.rates")
   }
   
 });
