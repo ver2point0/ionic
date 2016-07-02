fc13bb4a74dd987c3e205afab7f98e02c12e02d0
John McIntosh II <jmci10@hotmail.com>
2016-07-02 00:44:10 +0000

    add detail controller

--- chapter5/www/js/app.js
+++ chapter5/www/js/app.js
@@ -25,6 +25,15 @@ angular.module('App', ['ionic'])
         }
       }
     })
+    .state("tabs.detail", {
+      url: "/detail/:currency",
+      views: {
+        "rates-tab": {
+          templateUrl: "views/detail/detail.html",
+          controller: "DetailController"
+        }
+      }
+    })
     .state("tabs.currencies", {
       url: "/currencies",
       views: {
