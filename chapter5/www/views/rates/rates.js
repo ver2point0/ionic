a0d75596e5e453e04791e29ea634d7caeafcdecf
John McIntosh II <jmci10@hotmail.com>
2016-07-09 01:51:49 +0000

    register popover with controller

--- chapter5/www/views/rates/rates.js
+++ chapter5/www/views/rates/rates.js
@@ -1,7 +1,21 @@
 angular.module("App")
-.controller("RatesController", function($scope, $http, Currencies) {
+.controller("RatesController", function($scope, $http, $ionicPopover, Currencies) {
 
   $scope.currencies = Currencies;
+  
+  $ionicPopover.fromTemplateUrl("views/rates/help-popover.html", {
+    scope: $scope,
+  }).then(function (popover) {
+    $scope.popover = popover;
+  })
+  
+  $scope.openHelp = function($event) {
+    $scope.popover.show($event);
+  };
+  
+  $scope.$on('$destroy', function() {
+    $scope.popover.remove();
+  });
   
   $scope.load = function () {
     $http.get("https://api.bitcoinaverage.com/ticker/all").success(
