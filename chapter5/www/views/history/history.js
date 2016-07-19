ddc0fe47d71d4c6392d976b1cff15d28bd93b4e7
John McIntosh II <jmci10@hotmail.com>
2016-07-19 16:34:43 +0000

    continue history controller

--- chapter5/www/views/history/history.js
+++ chapter5/www/views/history/history.js
@@ -1,4 +1,37 @@
 angular.module("App")
 .controller("HistoryController", function ($scope, $http, $state, $stateParams, Currencies) {
   
+  $scope.history = {
+    currency: $stateParams.currency || "USD"
+  };
+  $scope.currencies = Currencies;
+  
+  $scope.changeCurrency = function () {
+    $state.go("tabs.history", { currency: $scope.history.currency });
+  };
+  
+  $scope.chart = {
+    options: {
+      chart: {
+        type: "line"
+      },
+      legend: {
+        enabled: false
+      }
+    },
+    title: {
+      text: null
+    },
+    yAxis: {
+      title: null
+    },
+    xAxis: {
+      type: "datetime"
+    },
+    series: []
+  };
+  
+  
+  
+  
 });
